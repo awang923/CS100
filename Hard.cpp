065c25adc2ff1026a45e73f30f23890ccad91fb2
isant028 <isabellasantiago2469@gmail.com>
2020-11-23 16:45:34 -0800

    started hard class

---
+++ Hard.cpp
@@ -0,0 +1,14 @@
+#ifndef __EASY_CPP__
+#define __EASY_CPP__
+
+#include "Board.hpp"
+#include "Difficulty.hpp"
+
+class Difficulty: public Hard {
+        public:
+             Hard() : Difficulty() {}
+             void difficulty (Board* board){
+	}
+}:
+
+#endif
