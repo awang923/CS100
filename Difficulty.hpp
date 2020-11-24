1a105304c67390759b55b08dfa95cc84a264863f
isant028 <isabellasantiago2469@gmail.com>
2020-11-23 16:32:55 -0800

    added difficulty abstract class

---
+++ Difficulty.hpp
@@ -0,0 +1,13 @@
+#ifndef __DIFFICULTY_HPP__
+#define __DIFFICULTY_HPP__
+#include "Board.hpp"
+class Board; 
+class Difficulty{
+        public:
+        Difficulty(){};
+        virtual void difficulty(Board* board) =0;
+
+};
+
+#endif
+~       
