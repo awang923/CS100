706f11abc6053488a8820d944cf507c5905582d0
jsegu447 <jsegu010@ucr.edu>
2020-12-02 23:13:13 -0800

    made random

--- Board.hpp
+++ Board.hpp
@@ -2,12 +2,15 @@
 #define BOARD_H
 //#include "Difficulty.hpp"
 #include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
 //class Difficulty;
 class Board {
 protected:
 	std::vector < std::vector<char> > set;
 	std::vector < std::vector<char> > displaySet;
-	
+	void populate();
 public:
 	void display();
         Board();
