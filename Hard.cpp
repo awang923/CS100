4c4c97ecbe5953f42b7a465a28418657930c74bc
isant028 <isabellasantiago2469@gmail.com>
2020-12-05 18:47:33 -0800

    added check to Hard.cpp

--- Hard.cpp
+++ Hard.cpp
@@ -1,13 +1,30 @@
-#ifndef __EASY_CPP__
-#define __EASY_CPP__
+#ifndef __HARD_CPP__
+#define __HARD_CPP__
 
 #include "Board.hpp"
 #include "Difficulty.hpp"
+#include <iostream>
+#include <vector>
+using namespace std;
 
 class Difficulty: public Hard {
         public:
-             Hard() : Difficulty() {}
-             void difficulty (Board* board){
+	     
+	     Hard() : Difficulty() {};
+	     Hard(Board* b) : Difficulty(b) {}
+             int y = YCoor -1;
+	     //int x = XCoor[1] - 97;
+	     int x = convert(XCoor);
+		void bool difficulty (){
+		if (gameBoard->set[0][1] == '*'){
+			cout << endl << "AaaARrrrGGgghhHH! You hit the pirate ship, better luck next time!" << endl;
+			this->displayPirate();
+			return false;
+		}
+		else{
+			displaySet[y][x] = set[y][x];
+			return true;
+		} 
 	}
 }:
 
