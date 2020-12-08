f1a93ac78bfcccbf5b8e2313846045f55a6f3f5b
awang923 <awang144@ucr.edu>
2020-12-07 20:45:29 -0800

    fixed bug

--- Board.cpp
+++ Board.cpp
@@ -65,12 +65,17 @@ void Board::getXCoor(char XCoor){
 
 void Board::getYCoor(int YCoor) {
    cout << "Please enter the Y coordinate." << endl;
-   cin >> YCoor;
-   while (!(YCoor <= 10 && YCoor >= 1)|| !(cin >> YCoor)){
-//	cin >> YCoor;
+  if (!(cin >> YCoor)){
+  cout << "Invalid input. Please enter a number between 1 and 9." << endl;
+        cin.clear();
+        cin.ignore(10000,'\n');
+        cin >> YCoor;
+  
+}
+   while (!(YCoor <= 10 && YCoor >= 1)){
         cout << "Invalid input. Please enter a number between 1 and 9." << endl;
-  //	cin.clear();
-//	cin.ignore(10000,'\n');
+  	cin.clear();
+	cin.ignore(10000,'\n');
 	cin >> YCoor;
 	break;
    }
